8c37224bb95958e9a3f702a2ebd7591baebbdf9f
Jasbir96 <mr.jasbirsingh96@gmail.com>
2024-08-28 09:08:59 +0530

    status and name update

--- src/Components/AuthContext.jsx
+++ src/Components/AuthContext.jsx
@@ -22,14 +22,15 @@ function AuthWrapper({ children }) {
                 const docRef = doc(db, "users", currentUser?.uid);
                 const docSnap = await getDoc(docRef);
                 if (docSnap.exists()) {
-                    const {profile_pic, email,name } = docSnap.data();
+                    const { profile_pic, email, name ,status} = docSnap.data();
                     console.log("26", docSnap.data());
                     // context me jaake save kr dia hai user ka data
                     setUserData({
                         id: currentUser?.uid,
                         profile_pic: profile_pic,
                         email,
-                        name
+                        name,
+                        status:status? status :""
                     });
                     updateLastSeen(currentUser);
                 }
@@ -49,13 +50,36 @@ function AuthWrapper({ children }) {
         });
     };
 
-    const logout =  () => {
+    const updateName = async (name) => {
+        await updateDoc(doc(db, "users", userData.id), {
+            displayName: name,
+        });
+        setUserData({
+            ...userData,
+            name: name,
+        });
+    };
+
+    const updateStatus = async (status) => {
+        await updateDoc(doc(db, "users", userData.id), {
+            status: status,
+        });
+        setUserData({
+            ...userData,
+            status: status,
+        });
+    };
+
+    const logout = () => {
         signOut(auth);
     };
 
     console.log("userData authcontext", userData);
     return <AuthContext.Provider value={{
-        setUserData, userData, loading,logout}}>
+        setUserData, userData, loading, logout,
+        updateName,
+        updateStatus,
+    }}>
         {children}
     </AuthContext.Provider>
 }
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,11 +1,13 @@
-import { ArrowLeft } from 'lucide-react'
-import React from 'react'
+import { ArrowLeft, CheckIcon } from 'lucide-react'
+import React, { useState } from 'react'
 import { useAuth } from './AuthContext'
 import { useNavigate } from 'react-router-dom';
 
 function Profile(props) {
     const navigate=useNavigate();
-    const { userData, logout } = useAuth();
+    const { userData, logout, updateName,updateStatus } = useAuth();
+    const [name, setName] = useState(userData?.name||"");
+    const [status, setStatus] = useState(userData?.status||"");
     const handleLogout = () => {
         logout();
         navigate("/login");
@@ -23,10 +25,40 @@ function Profile(props) {
             <div className="bg-gray-100">
                 <img src={userData.profile_pic} alt="" className="rounded-full h-10 w-10 " />
                 {/* ... other user data */}
-                <h2>{userData.name}</h2>
-                <h2>{userData.email}</h2>
+                <div className="flex flex-col bg-white w-full py-4 px-8">
+                    <label className="text-sm text-primary-dense mb-2">Your name</label>
+                    <div className="flex items-center w-full">
+                        <input
+                            value={name}
+                            onChange={(e) => {
+                                setName(e.target.value);
+                            }}
+                            className="w-full bg-transparent"
+                        />
+                        <button onClick={() => updateName(name)}>
+                            <CheckIcon className="w-5 h-5" />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="flex flex-col bg-white  w-full py-4 px-8">
+                    <label className="text-sm text-primary-dense mb-2">Status</label>
+                    <div className="flex items-center w-full">
+                        <input
+                            value={status}
+                            onChange={(e) => {
+                                setStatus(e.target.value);
+                            }}
+                            placeholder="Update your status..."
+                            className="w-full bg-transparent"
+                        />
+                        <button onClick={() => updateStatus(status)}>
+                            <CheckIcon className="w-5 h-5" />
+                        </button>
+                    </div>
+                </div>
                 <button
-                    className="mt-8 px-4 py-3 rounded bg-primary hover:bg-primaryDense text-white"
+                    className="mt-8 px-4 py-3 rounded bg-primary hover:bg-primary-dense text-white"
                     onClick={handleLogout}
                 >
                     Logout
